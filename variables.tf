db08662197528c0f43d50888ed2c2bb10ac30881
Camus Ulloa Jonsson <camus@perxhealth.com>
2022-12-02 16:49:36 +1100

    remove lambda, add policy support

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
-variable "name_prefix" {
-  description = "The prefix for names of created resources"
+variable "name" {
+  description = "The name of created resources"
   type        = string
 }
 
@@ -66,56 +66,6 @@ variable "tags" {
   default     = {}
 }
 
-variable "rotation_lambda_handler" {
-  description = "An optional lambda handler name; useful integration with for certain layer providers"
-  type        = string
-  default     = null
-}
-
-variable "rotation_lambda_env_variables" {
-  description = "Optional environment variables for the rotation lambda; useful for integration with for certain layer providers"
-  type        = map(string)
-  default     = {}
-}
-
-variable "rotation_lambda_policy_jsons" {
-  description = "Additional policies to add to the rotation lambda; useful for integration with layer providers"
-  type        = list(string)
-  default     = []
-}
-
-variable "rotation_lambda_layers" {
-  description = "Optional layers for the rotation lambda."
-  type        = list(string)
-  default     = null
-}
-
-variable "rotation_lambda_subnet_ids" {
-  description = "The VPC subnets that the rotation lambda runs in. Required for secret rotation."
-  type        = list(string)
-  default     = []
-}
-
-variable "rotation_lambda_vpc_id" {
-  description = "The VPC that the secret rotation lambda runs in. Required for secret rotation."
-  type        = string
-  default     = null
-}
-
-variable "db_security_group_id" {
-  description = "The security group ID for the database. Required for secret rotation."
-  type        = string
-  default     = null
-}
-
-variable "recreate_missing_package" {
-  description = "Whether to recreate missing Lambda package if it is missing locally or not"
-  type        = bool
-  default     = true
-}
-
-variable "role_permissions_boundary" {
-  description = "Optional permissions boundary for rotation lambda IAM role."
-  type        = string
-  default     = null
+variable "role_arns" {
+  type = list(string)
 }
